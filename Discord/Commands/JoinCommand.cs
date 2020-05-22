9a1cce25fffca8ae8a0cb67c88b2bb13b413bdd9
Jon Fus <jonfus123@hotmail.com>
2020-05-22 00:07:25 -0400

    Added join and leave queue commands. removed test command

---
+++ Discord/Commands/JoinCommand.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using InhouseBot.Matchmaking;
+using InhouseBot.Mysql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhouseBot.Discord.Commands
+{
+    public class JoinCommand : ModuleBase<SocketCommandContext>
+    {
+
+
+        [Command("join")]
+        [Summary("Joins the in-house queue")]
+        public async Task JoinDefaultAsync()
+        {
+            SocketUser sender = Context.User;
+
+            UserData user = new UserData(sender.Id);
+            bool rs = await user.Fetch();
+
+            if (rs)
+            {
+                Console.WriteLine(user.err);
+                return;
+            }
+
+            MatchPlayer player = new MatchPlayer(user);
+            bool didJoin = MatchMaker.Join(player);
+
+            if (!didJoin)
+                return;
+
+            await ReplyAsync($"{sender.Username} has joined the queue! Position {player.joinSlot}");
+        }
+    }
+}
---
+++ Discord/Commands/LeaveCommand.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using InhouseBot.Matchmaking;
+using InhouseBot.Mysql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhouseBot.Discord.Commands
+{
+    public class LeaveCommand : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("leave")]
+        [Summary("Leaves the in-house queue")]
+        public async Task LeaveDefaultAsync()
+        {
+            MatchMaker.Leave(Context.User.Id);
+            await ReplyAsync($"{Context.User.Username} has left the queue!");
+        }
+    }
+}
